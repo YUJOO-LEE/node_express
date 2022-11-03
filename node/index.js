cfea43724737ddedf4c2b4a26beb9f2c8c379e16
YUJOO-LEE <lllllllllee@gmail.com>
2022-11-03 14:38:08 +0900

    react 빌드, 서버와 연결

--- node/index.js
+++ node/index.js
@@ -5,14 +5,28 @@
 // node index.js
 // 변경점 생길때마다 재구동 필요
 
+//const { request, path } = require('express');
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8888;
+/*
 
-app.get('/', (request, response)=>{
-  response.send('Hello World');
-});
+express에서 react안쪽 build 폴더까지의 경로를 static으로 지정
+__dirname : 현재 경로
+
+*/
+app.use(express.static(path.join(__dirname, '../react/build')));
 
 app.listen(port, ()=>{
   console.log(`Server app listening on port ${port}`);
+});
+
+app.get('/', (request, response)=>{
+  //response.send('Hello World');
+  response.sendFile(path.join(__dirname, '../react/build/index.html'));
+});
+
+app.get('*', (request, response)=>{
+  response.sendFile(path.join(__dirname, '../react/build/index.html'));
 });
