0684da860be342cd9649af5185aa74224bef065e
YUJOO-LEE <lllllllllee@gmail.com>
2022-11-10 08:56:44 +0900

    detail 디자인 완료

--- App/react/src/community/Detail.js
+++ App/react/src/community/Detail.js
@@ -5,15 +5,46 @@ import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import Layout from '../common/Layout';
 
-const DetailWrap = styled.div`
+const ArticleHeader = styled.div`
+  width: 100%;
+  margin-bottom: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  h2,p{
+    display: flex;
+    flex-direction: column;
+    &:last-child{
+      text-align: right;
+    }
+    .title{
+      font-size: 36px;
+      font-weight: 100;
+    }
+    .writer{
+      font-size: 18px;
+      font-weight: 500;
+    }
+    span{
+      font-size: 12px;
+      font-weight: 100;
+    }
+    strong{
+      font-weight: 500;
+    }
+  }
+`;
+
+const Content = styled.div`
   width: 100%;
   padding: 40px;
-  background: #fff;
-  box-shadow: 10px 10px 20px rgba(0,0,0,0.2);
+  background: var(--color-white);
+  border-radius: 10px;
+  font-weight: 100;
 `;
 
 const BtnSet = styled.div`
-  margin-top: 20px;
+  margin-top: 40px;
 
   button{
     margin-right: 20px;
@@ -67,14 +98,29 @@ function Detail() {
     <>
       {Loaded ? Detail &&
         <Layout name='Detail'>
-          <DetailWrap>
-            <h2>{Detail.title}</h2>
-            <p>{Detail.writer.displayName}</p>
+          <ArticleHeader>
+            <h2>
+              <span className='title'>
+                {Detail.title}
+              </span>
+              <span className='writer'>
+                {Detail.writer.displayName}
+              </span>
+            </h2>
             <p>
-              {Detail.createdAt.split('T')[0]}
+              <span>
+                <strong>posted</strong> {Detail.createdAt.slice(0,19).replace('T', ' ')}
+              </span>
+              {Detail.createdAt !== Detail.updatedAt &&
+                <span>
+                  <strong>updated</strong> {Detail.updatedAt.slice(0,19).replace('T', ' ')}
+                </span>
+              }
             </p>
-            <p>{Detail.content}</p>
-          </DetailWrap>
+          </ArticleHeader>
+          <Content>
+            {Detail.content}
+          </Content>
 
           {User.uid === Detail.writer.uid &&
             <BtnSet>
